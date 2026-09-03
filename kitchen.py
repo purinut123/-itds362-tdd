df81801c94d842eb5259a8e25a98601b59a2e217
purinut123 <purinut.khi@student.mahidol.ac.th>
2026-09-03 19:44:31 +0700

    A4: ความเท่ากัน (Equality)

--- kitchen.py
+++ kitchen.py
@@ -3,6 +3,12 @@ class Quantity:
     def __init__(self, amount):
         self.amount = amount
 
+    def __eq__(self, other):
+        return self.amount == other.amount
+ 
+    def __repr__(self):
+        return f"Quantity({self.amount})"
+
     def times(self, multiplier):
         return Quantity(self.amount * multiplier)
 
--- test_kitchen.py
+++ test_kitchen.py
@@ -11,15 +11,20 @@ from kitchen import Quantity
  
 def test_multiplication():
     flour = Quantity(200)
-    assert flour.times(3).amount == 600
+    assert flour.times(3) == Quantity(600)
 
 def test_multiplication_by_two():
     flour = Quantity(200)
-    assert flour.times(2).amount == 400
+    assert flour.times(2) == Quantity(400)
 
 def test_multiplication_returns_a_new_quantity():
     flour = Quantity(200)
-    assert flour.times(3).amount == 600
-    assert flour.times(2).amount == 400
+    assert flour.times(3) == Quantity(600)
+    assert flour.times(2) == Quantity(400)
+
+def test_equality():
+    assert Quantity(200) == Quantity(200)
+    assert Quantity(200) != Quantity(300)
+
 
 
