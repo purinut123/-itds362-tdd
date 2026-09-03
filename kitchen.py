26a278e3e0925efc861e16fd4c679e0f3479676b
purinut123 <purinut.khi@student.mahidol.ac.th>
2026-09-03 19:34:13 +0700

    A3: REFACTOR

--- kitchen.py
+++ kitchen.py
@@ -2,10 +2,8 @@
 class Quantity:
     def __init__(self, amount):
         self.amount = amount
- 
-    def times(self, multiplier):
-        #self.amount = 600
 
     def times(self, multiplier):
-        self.amount = self.amount * multiplier
+        return Quantity(self.amount * multiplier)
+
 
--- test_kitchen.py
+++ test_kitchen.py
@@ -11,11 +11,15 @@ from kitchen import Quantity
  
 def test_multiplication():
     flour = Quantity(200)
-    flour.times(3)
-    assert flour.amount == 600
+    assert flour.times(3).amount == 600
 
 def test_multiplication_by_two():
     flour = Quantity(200)
-    flour.times(2)
-    assert flour.amount == 400
+    assert flour.times(2).amount == 400
+
+def test_multiplication_returns_a_new_quantity():
+    flour = Quantity(200)
+    assert flour.times(3).amount == 600
+    assert flour.times(2).amount == 400
+
 
